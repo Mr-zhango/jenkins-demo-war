ed03579e3c6e93589fe2927cb11eaef68a945547
zhangyang <zhangyang02@wanxue.cn>
2020-07-24 11:24:32 +0800

    init commit

---
+++ src/main/java/cn/myfreecloud/war/controller/HelloControlelr.java
@@ -0,0 +1,14 @@
+package cn.myfreecloud.war.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControlelr {
+
+    @GetMapping("hello")
+    public ResponseEntity hello() {
+        return ResponseEntity.ok("Hello jenkins war");
+    }
+}
