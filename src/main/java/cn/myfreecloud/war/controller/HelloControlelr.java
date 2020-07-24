88148716221fb6756ab198dbdc2116c8719afc09
zhangyang <zhangyang02@wanxue.cn>
2020-07-24 17:19:50 +0800

    添加邮件发送模板

--- src/main/java/cn/myfreecloud/war/controller/HelloControlelr.java
+++ src/main/java/cn/myfreecloud/war/controller/HelloControlelr.java
@@ -9,6 +9,6 @@ public class HelloControlelr {
 
     @GetMapping
     public ResponseEntity hello() {
-        return ResponseEntity.ok("Hello jenkins war Pipeline dev分支");
+        return ResponseEntity.ok("Hello jenkins war Pipeline master分支");
     }
 }
